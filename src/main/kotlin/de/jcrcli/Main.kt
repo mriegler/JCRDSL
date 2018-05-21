663c001a7c8dbcc990e9e2665cc5d05cfd863561
LinXitoW <mriegler@gmx.de>
2018-05-21 16:05:15 +0200

    add rough draft of JCR node DSL
    remove test code

--- src/main/kotlin/de/jcrcli/Main.kt
+++ src/main/kotlin/de/jcrcli/Main.kt
@@ -1,23 +1,16 @@
 package de.jcrcli
 
-import com.authzee.kotlinguice4.getInstance
-import com.google.inject.Guice
-import com.google.inject.Inject
-import mu.KLogging
 import mu.KotlinLogging
+import org.apache.jackrabbit.commons.SimpleValueFactory
 import org.apache.jackrabbit.core.RepositoryImpl
 import org.apache.jackrabbit.core.config.RepositoryConfig
 import javax.jcr.Node
 import javax.jcr.SimpleCredentials
+import javax.jcr.Value
 
 private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
     logger.debug { "Starting main with args $args, size: ${args.size}" }
-    val injector = Guice.createInjector(Module())
-    val publicRelations = injector.getInstance<PublicRelations>()
-
-    publicRelations.handleUser("Marcel")
-
     val classLoader = Thread.currentThread().contextClassLoader
     val conf = classLoader.getResourceAsStream("repo.xml")
 
@@ -28,30 +21,95 @@ fun main(args: Array<String>) {
     val root = session.rootNode
     root.addNode("test").setProperty("count", 1)
     logger.info { "Got repo: $repo, node: ${root.getNode("test")}" }
+    val definition = node {
+        name = "masternode"
+
+        property {
+            name = "testProp"
+            value = SimpleValueFactory().createValue("Stringval")
+        }
+        node {
+            name = "childnode"
+            primaryType = "nt:unstructured"
+
+            property {
+                name = "childProp"
+                value = SimpleValueFactory().createValue(true)
+            }
+        }
+    }
+
+    logger.info("Definition is: $definition")
+
+    val attachedNode = root.attach(definition)
+
+    logger.info("Attached node $attachedNode, node name ${attachedNode.name}")
+
 }
 
-object node {
-    operator fun invoke(node: Node, init: Node.() -> Unit) {
+fun Node.attach(definition: NodeDefinition): Node {
+    return definition.attach(this)
+}
+
+fun node(setup: NodeDefinitionBuilder.() -> Unit): NodeDefinition {
+    val nodeDefinitionBuilder = NodeDefinitionBuilder()
+    nodeDefinitionBuilder.setup()
+    return nodeDefinitionBuilder.build()
+}
+
+class NodeDefinitionBuilder {
+    val props = mutableListOf<PropertyDefinition>()
+    val children = mutableListOf<NodeDefinition>()
+    val mixins = mutableListOf<String>()
+    var primaryType = "nt:unstructured"
+    var name: String? = null
+
+    fun build(): NodeDefinition {
+        val name = name ?: throw IllegalStateException("a node definition requires a name")
+
+        return NodeDefinition(name, primaryType, mixins, props, children)
+    }
+
+    fun property(setup: PropertyDefinitionBuilder.() -> Unit) {
+        val builder = PropertyDefinitionBuilder()
+        builder.setup()
+        props += builder.build()
+    }
+
+    fun node(setup: NodeDefinitionBuilder.() -> Unit) {
+        val builder = NodeDefinitionBuilder()
+        builder.setup()
+        children += builder.build()
     }
 }
 
-class PublicRelations @Inject constructor(val greeter: Greeter) {
-    fun handleUser(name: String) {
-        logger.debug { "PR handling user $name" }
-        println(greeter.greet(name))
+class PropertyDefinitionBuilder {
+    var name: String = ""
+    var value: Value = SimpleValueFactory().createValue(false)
+    fun build(): PropertyDefinition {
+        return PropertyDefinition(name, value)
     }
-
-    companion object : KLogging()
 }
 
-interface Greeter {
-    fun greet(name: String): String
+data class NodeDefinition(
+    val name: String,
+    val primaryType: String,
+    val mixins: List<String>,
+    val properties: List<PropertyDefinition>,
+    val children: List<NodeDefinition>
+) {
+    fun attach(parent: Node): Node {
+        val newNode = parent.addNode(name, primaryType)
+        mixins.forEach { newNode.addMixin(it) }
+        properties.forEach { newNode.setProperty(it.name, it.value) }
+        children.forEach { it.attach(newNode) }
+
+        return newNode
+    }
 }
 
-class RudeGreeter : Greeter {
-    override fun greet(name: String) = "Hi, $name, you fatass!"
-}
+data class PropertyDefinition(
+    val name: String,
+    val value: Value
+)
 
-class NiceGreeter : Greeter {
-    override fun greet(name: String): String = "Hello, my dear $name"
-}
--- src/main/kotlin/de/jcrcli/Module.kt
+++ src/main/kotlin/de/jcrcli/Module.kt
@@ -1,14 +1,7 @@
 package de.jcrcli
 import com.authzee.kotlinguice4.KotlinModule
-import java.util.*
 
 class Module : KotlinModule() {
     override fun configure() {
-        val evenTime = Calendar.getInstance().timeInMillis.rem(2) == 0L
-        if (evenTime) {
-            bind<Greeter>().to<NiceGreeter>()
-        } else {
-            bind<Greeter>().to<RudeGreeter>()
-        }
     }
 }
