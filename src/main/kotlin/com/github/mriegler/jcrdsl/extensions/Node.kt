bce5bd23068f92c9ee0bfef253e70b23e48e7677
LinXitoW <mriegler@gmx.de>
2018-05-23 23:58:19 +0200

    add rough ideas for new functions

--- src/main/kotlin/com/github/mriegler/jcrdsl/extensions/Node.kt
+++ src/main/kotlin/com/github/mriegler/jcrdsl/extensions/Node.kt
@@ -3,6 +3,38 @@ package com.github.mriegler.jcrdsl.extensions
 import com.github.mriegler.jcrdsl.models.NodeDefinition
 import javax.jcr.Node
 
+/**
+ * Creates and attaches nodes according to the [definition], returning the given [Node] for chaining.
+ *
+ * The top level node definition does not refer to the receiver, but
+ * to a child to be created
+ */
 fun Node.attach(definition: NodeDefinition): Node {
     return definition.attach(this)
+}
+
+/**
+ * Ensures the given [node] matches the [definition] by making changes
+ * where needed, returning the given node for chaining
+ *
+ * The top level node definition refers to the receiver node
+ */
+fun Node.ensure(definition: NodeDefinition): Node {
+    TODO()
+}
+
+/**
+ * Compares the [Node] to the [definition], returning true if they match.
+ */
+fun Node.matches(definition: NodeDefinition): Boolean {
+    TODO()
+}
+
+/**
+ * Compares the [Node] to the [definition], returning the difference.
+ *
+ * TODO: Figure out use case. Do we need more than [Node.matches]?
+ */
+fun Node.compare(definition: NodeDefinition): Node {
+    TODO()
 }
