810066e54da69d4b5d4636e88206b6e1a6e86761
Marcel Riegler <mriegler@gmx.de>
2018-05-28 00:16:19 +0200

    add convenience constructors to property def

--- src/main/kotlin/com/github/mriegler/jcrdsl/models/PropertyDefinition.kt
+++ src/main/kotlin/com/github/mriegler/jcrdsl/models/PropertyDefinition.kt
@@ -1,8 +1,28 @@
 package com.github.mriegler.jcrdsl.models
 
+import org.apache.jackrabbit.commons.SimpleValueFactory
+import java.io.InputStream
+import java.math.BigDecimal
+import java.util.*
+import javax.jcr.Node
 import javax.jcr.Value
+import javax.jcr.PropertyType
 
 data class PropertyDefinition(
     val name: String,
     val value: Value
-)
+) {
+    constructor(name: String, value: String) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: Boolean) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: Long) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: InputStream) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: Calendar) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: Node) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: Double) : this(name, SimpleValueFactory().createValue(value))
+    constructor(name: String, value: BigDecimal) : this(name, SimpleValueFactory().createValue(value))
+
+    /**
+     * Delegates to [SimpleValueFactory.createValue] with [type] coming from [PropertyType]
+     */
+    constructor(name: String, value: String, type: Integer) : this(name, SimpleValueFactory().createValue(value, type.toInt()))
+}
