f9ce2c791d4edfd093dc4e27d1f6f05e43e43826
Marcel Riegler <mriegler@gmx.de>
2018-05-29 13:32:40 +0200

    add basic dsl tests

---
+++ src/test/kotlin/com/github/mriegler/jcrdsl/DslTest.kt
@@ -0,0 +1,30 @@
+package com.github.mriegler.jcrdsl
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class DslTest : StringSpec({
+    "writing the same dsl twice should result in the same definition" {
+        val def1 = node {
+            name = "test"
+            property("test", "test")
+        }
+        val def2 = node {
+            name = "test"
+            property("test", "test")
+        }
+
+        def1 shouldBe def2
+    }
+
+    "definitions should be the same, no matter whether they are a subnode or not" {
+        val def = node {
+            name = "test"
+            node {
+                name = "child"
+            }
+        }
+
+        def.children[0] shouldBe node { name = "child" }
+    }
+})
