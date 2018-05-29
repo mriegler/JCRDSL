f2a9f2cbda330887b2e2cc3336091567cd6928cb
Marcel Riegler <mriegler@gmx.de>
2018-05-29 14:37:26 +0200

    add unit tests for new methods (todo)

--- src/test/kotlin/com/github/mriegler/jcrdsl/extensions/NodeTest.kt
+++ src/test/kotlin/com/github/mriegler/jcrdsl/extensions/NodeTest.kt
@@ -2,6 +2,7 @@ package com.github.mriegler.jcrdsl.extensions
 
 import com.github.mriegler.jcrdsl.helpers.session
 import com.github.mriegler.jcrdsl.node
+import io.kotlintest.matchers.boolean.shouldBeTrue
 import io.kotlintest.should
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
@@ -18,6 +19,41 @@ class NodeTest : StringSpec({
         newNode.getProperty("test").value should {
             it.string shouldBe "test"
         }
+    }
 
+    "matches should compare the nodes correctly" {
+        val newNode = session.rootNode.addNode("master", "nt:unstructured")
+        newNode.setProperty("test", "test")
+
+        newNode.matches(node {
+            name = "master"
+            property("test", "test")
+        })
+    }
+
+    "ensure should create missing nodes and properties" {
+        val newNode = session.rootNode.addNode("test", "nt:unstructured")
+        newNode.setProperty("test", "test")
+        val child = newNode.addNode("child")
+        child.setProperty("test", 3)
+
+        val def = node {
+            name = "test"
+            primaryType = "nt:folder"
+            property("test2", "test2")
+            node {
+                name = "child"
+                property("test", 3)
+            }
+        }
+
+        newNode.ensure(def)
+
+        newNode.name shouldBe "test"
+        newNode.primaryNodeType.isNodeType("nt:folder").shouldBeTrue()
+        newNode.getProperty("test").string shouldBe "test"
+        newNode.getProperty("test2").string shouldBe "test2"
+        newNode.hasNode("child").shouldBeTrue()
+        newNode.getNode("child").getProperty("test").long shouldBe 3
     }
 })
